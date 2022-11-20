3f88d75f738e62b733d5ed71e9b170a62a5ec768
GuilleGarrido <ggarridoluque@gmail.com>
2022-11-20 13:04:28 +0100

    2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const v1TenisRouter = require('./v1/routes/tenisRouter');
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/api/v1/tenis", v1TenisRouter);
